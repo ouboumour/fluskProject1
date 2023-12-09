4cfb2a482934617f3c7974ab4babbe962d6dfdb9
ouboumour <oussamaboumour7@gmail.com>
2023-12-09 22:44:49 +0100

    init project

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/marwa')
+def marwa():  # put application's code here
+    return 'Hello Marwa!'
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
